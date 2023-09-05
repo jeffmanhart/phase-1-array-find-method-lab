96527517cac7708cf0e93ef6112de397c1605418
jeffmanhart <jeffreymanhart@gmail.com>
2023-09-05 13:18:29 -0600

    found superbowl wins

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(item){
+    const winObj = item.find(isSuperbowlWin)
+    if(winObj != null){
+        return winObj.year
+    }
+}
+const isSuperbowlWin = check => check.result === "W";
+console.log(superbowlWin(record))
+
